406455654570902e3b986abc484b70a2f23044c8
Alex Kinyua <alexkinyua001alx@gmail.com>
2023-11-25 00:07:34 -0800

    fixed task 1 flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,4 +3,16 @@
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  name     => 'flask',
+  require  => Package['python3-pip'],
+}
+
+package { 'python3-pip':
+  ensure => present,
+}
+
+package { 'Werkzeug':
+  ensure   => '2.2.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
